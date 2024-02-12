daf1897dfd39c75e68a559561d404555d66b4d0a
jeevan.n0520@gmail.com <kiran11@gmail.com>
2024-02-12 15:43:48 +0530

    change1

---
+++ git.java
@@ -0,0 +1,6 @@
+class git{
+        public static void main(string[] args)
+        {
+                system.out.println("hello welcome to git hub")
+        }
+
---
+++ one.java
@@ -0,0 +1,7 @@
+public class hi frnds
+{
+	public static void main (String []args)
+	{
+		System.out.println("welcom to another world");
+	}
+}
