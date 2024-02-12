14597925dd3472287e8d92809cc2beed7332e757
jeevann0520 <159773778+jeevann0520@users.noreply.github.com>
2024-02-12 16:33:56 +0530

    Update git.java

--- git.java
+++ git.java
@@ -2,7 +2,7 @@ class git{
         public static void main(string[] args)
         {
                 system.out.println("hello welcome to git hub");
-		 system.out.println("created on 12-2-2024");
-
-        }
+		system.out.println("created on 12-2-2024");
+		system.out.println("after editing in git hub");
+	}
 
